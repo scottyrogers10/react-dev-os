d05a167fb47c6193feeefc0dc40a67d660da686c
Scotty Rogers <scotty.rogers10@gmail.com>
2020-12-26 22:50:48 -0700

    Updated colors of hot key labels.

--- src/components/DevOS/children/ToolsManager/children/ToolBrowser/children/ToolList/Item/HotKeyLabel/children/Label/styles.js
+++ src/components/DevOS/children/ToolsManager/children/ToolBrowser/children/ToolList/Item/HotKeyLabel/children/Label/styles.js
@@ -2,11 +2,11 @@ import { colors } from "@library/styles";
 
 export default {
   view: {
-    backgroundColor: colors.gray[200],
-    border: `1px solid ${colors.gray[500]}`,
+    backgroundColor: colors.gray[850],
+    border: `1px solid ${colors.gray[750]}`,
     borderRadius: "3px",
-    boxShadow: "0 1px 1px rgba(12,13,14,0.15), inset 0 1px 0 0 #fff",
-    color: colors.gray[700],
+    boxShadow: "0 1px 1px rgba(12,13,14,0.15), inset 0 1px 0 0 #616161",
+    color: colors.gray[200],
     fontSize: "9px",
     fontWeight: 600,
     minWidth: "17px",
