1482fb0ebcb8db5b811ee6e559171d8df5946d2c
Scotty Rogers <scotty.rogers10@gmail.com>
2020-12-29 22:46:10 -0700

    Updated default tool icon.

--- src/components/DevOS/children/ToolsManager/children/ToolBrowser/children/ToolList/Item/HotKeyLabel/children/Label/styles.js
+++ src/components/DevOS/children/ToolsManager/children/ToolBrowser/children/ToolList/Item/HotKeyLabel/children/Label/styles.js
@@ -5,7 +5,7 @@ export default {
     backgroundColor: colors.gray[850],
     border: `1px solid ${colors.gray[750]}`,
     borderRadius: "3px",
-    boxShadow: "0 1px 1px rgba(12,13,14,0.15), inset 0 1px 0 0 #616161",
+    boxShadow: "0 1px 1px rgba(12,13,14,0.15), inset 0 1px 0 0 #4e4e4e",
     color: colors.gray[200],
     fontSize: "9px",
     fontWeight: 600,
